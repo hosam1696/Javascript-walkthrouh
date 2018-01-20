72ecf2c36038840156d72f898756f4d4a8c1b9a5
hosamsam <hosamelnabawy1996@gmail.com>
2018-01-20 06:14:26 +0200

    :one: add some generators functions

--- node/ynlist/stream.js
+++ node/ynlist/stream.js
@@ -1,15 +1,27 @@
 // js generator <=> array
 const { log } = console
-
+const rand = () => Math.floor(Math.random()*10)+1
 function* values(min = 0, max = 10) {
     for (let i = min; i <= max; i++) {
         yield i
     }
 }
 
+function* genYear(to = new Date().getFullYear(), from = to - 3) {
+    for (let y = from; y <= to; y++) yield y
+}
+console.log(Array.from(genYear()))
+console.log(Array.from(genYear(), (iter, index) => iter))
+console.log(Array.from(genYear(), (iter, index) =>  iter + index ))
+console.log(Array.from(genYear(), (iter, index) => 'year:' + iter))
+console.log(Array.from(genYear(), (iter, index) => `i won ${rand()} prizes in ${iter}`))
+
+
+
+
 const valueArrays = Array.from(values(1, 10))
-let v = values() 
-log('Arrays values',valueArrays)
+let v = values()
+log('Arrays values', valueArrays)
 log(v.next().value)
 log(v.next().value)
 log(v.next().value)
@@ -22,4 +34,4 @@ const range = (from = 1, to = 10, predicate = defaultPredicate) => {
     return Array.from(values(from, to), predicate)
 }
 
-log('Odd Numbers',range(2,10, (v, k)=>v+k))
+log('Odd Numbers', range(2, 10, (v, k) => v + k))
